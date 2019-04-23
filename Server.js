05445b1f69ff856919f83f75234223dda1d51c31
Aaron <horwathhk@gmail.com>
2019-04-23 21:56:10 +0700

    git prepare deploy

--- Server.js
+++ Server.js
@@ -3,7 +3,7 @@ const graphqlHTTP = require("express-graphql");
 const data = require("./data/data.js");
 const cors = require("cors");
 const app = express();
-
+const path = require('path')
 //GRAPHQL
 //allow cross-origin access
 app.use("*", cors({ origin: "http://localhost:3000", credentials: true }));
@@ -17,6 +17,15 @@ app.use(
     };
   })
 );
+
+//serve statis assets if in production
+if(process.env.NODE_ENV === "production"){
+  //set static folder
+  app.use(express.static('client/travel-picker'))
+  app.get('*', (req,res) =>{
+    res.sendFile(path.resolve(__dirname, 'client', 'travel-picker', 'index.html' ))
+  }
+}
 
 app.listen(process.env.PORT || 4000);
 console.log("app running on port ", 4000);
